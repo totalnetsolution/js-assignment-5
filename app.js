a435099ee1c0e72d08428eceb75ff8255a9d1195
muhammad-danish <totalnetsolution2@gmail.com>
2024-02-28 01:40:30 +0500

    index file created

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
-var username =("Muhammad danish")
-alert(username)
+var username ="Muhammad danish";
+alert("My Name is "+username);
 
-var age =(40)
-alert("My age is +age+ years old")
+var age =40;
+alert("My age is "+age+" years old")
 
+var institute ="SMIT Batch 11";
+alert("My Batch is "+ institute);
 
-alert(institute)
-var institute =("SMIT Batch 11")
-
-alert(learn)
-var learn =("Web & Mobile App Development")
+var learn ="Web & Mobile App Development";
+alert("My Subject is "+learn);
 
 alert("PIZZA\nPIZZ\nPIZ\nPI\nP")
 
-var email =("totalnetsolution2@gmail.com")
+var email ="totalnetsolution2@gmail.com";
 alert("My email address is "+email);
 
-alert("You will be a Software Develpoer in Pakistan, and married")
+alert("I wanna be a Software Develpoer in Pakistan, and i am married")
 
 var book = ("A smarter way to learn JavaScript");
 alert("I am trying to learn from the Book "+book);
