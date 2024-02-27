4151aa67a6a2e0bfcd1b99bed98185e268547812
muhammad-danish <totalnetsolution2@gmail.com>
2024-02-28 01:23:00 +0500

    index file created

---
+++ app.js
@@ -0,0 +1,24 @@
+var username =("Muhammad danish")
+alert(username)
+
+var age =(40)
+alert("My age is +age+ years old")
+
+
+alert(institute)
+var institute =("SMIT Batch 11")
+
+alert(learn)
+var learn =("Web & Mobile App Development")
+
+alert("PIZZA\nPIZZ\nPIZ\nPI\nP")
+
+var email =("totalnetsolution2@gmail.com")
+alert("My email address is "+email);
+
+alert("You will be a Software Develpoer in Pakistan, and married")
+
+var book = ("A smarter way to learn JavaScript");
+alert("I am trying to learn from the Book "+book);
+
+document.write("Yay! i can write HTML content through Javascript")
